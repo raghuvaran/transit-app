320a7b0734532d758308ad2d1564d91cbcffb269
Raghuvaran <rchowda@g.clemson.edu>
2017-11-16 23:31:55 -0500

    modified logic to handle multiple buses

--- app/pods/index/controller.js
+++ app/pods/index/controller.js
@@ -1,5 +1,8 @@
 import Controller from '@ember/controller';
 import EmberObject from '@ember/object';
+import {inject} from '@ember/service';
+import { A as emberArray } from '@ember/array';
+import { isPresent } from '@ember/utils';
 import { computed } from '@ember/object';
 import { task, timeout } from 'ember-concurrency';
 import { fetchJson } from 'transit-app/utils/fetch';
@@ -12,17 +15,29 @@ const busObject = EmberObject.extend({
     const { userLat, userLng, lat, lng } = this.getProperties('userLat', 'userLng', 'lat', 'lng');
     const coordArray =  [userLat, userLng, lat, lng];
     return coordArray.every(v => !!v) && getDistanceFromLatLonInMiles(userLat, userLng, lat, lng);
+  }),
+
+  directionIcon: computed('direction', function() {
+    const direction = this.get('direction');
+    return {
+      'northbound': 'arrow upward',
+      'southbound': 'arrow downward',
+      'eastbound' : 'arrow forward',
+      'westbound' : 'arrow back',
+    }[direction];
   })
 });
 
 export default Controller.extend({
+  globals: inject('globals'),
   busURL: `${padding}http://developer.itsmarta.com/BRDRestService/RestBusRealTimeService/GetAllBus`,
-  buses: [
+  requestedBuses: [
     ["16", "southbound"],
     ["99", "northbound"],
     ["109", "northbound"],
     
-  ].map(b => busObject.create({route: b[0], direction: b[1]})),
+  ].reduce((a,c) => {a[c[0]]=[c[1]]; return a},{}),
+  activeBuses: emberArray(),
   // routes: EmberObject.computed.mapBy('buses', 'route'),
   currentLocation: null,
   refreshInterval: null,
@@ -38,6 +53,7 @@ export default Controller.extend({
   
   async getAllResponse() {
     const response = await fetchJson(this.get('busURL'));
+    this.set('globals.lastPolledAt', moment().toDate());
     return JSON.parse(response.body);
   },
 
@@ -55,17 +71,38 @@ export default Controller.extend({
         let [userLat, userLng] = [this.get('currentLocation')[0], this.get('currentLocation')[1]];
         let response = yield this.getAllResponse();
 
-        response.forEach(trip => {
-            const bus = this.get('buses').findBy('route', trip.ROUTE);
-            if(!bus || bus.get('direction') !== trip.DIRECTION.toLowerCase()) return;
+        response.forEach(b => {
+          const allowedBus = this.get('requestedBuses')[b.ROUTE];
+          // skipi if bus doesn't match the criterion
+          if(!allowedBus || allowedBus[0] !== String(b.DIRECTION).toLowerCase()) return;
 
-            bus.setProperties({
-              userLat, userLng,
-              lat: trip.LATITUDE,
-              lng: trip.LONGITUDE,
-              adherence: trip.ADHERENCE,
-              msgTime: trip.MSGTIME,
-            });
+          const bus = this.get('activeBuses').findBy('blockAbbr', b.BLOCK_ABBR);
+          const attrs = {
+            userLat,
+            userLng,
+            route: b.ROUTE,
+            direction: String(b.DIRECTION).toLowerCase(),
+            blockAbbr: b.BLOCK_ABBR,
+            lat: b.LATITUDE,
+            lng: b.LONGITUDE,
+            adherence: b.ADHERENCE,
+            msgTime: b.MSGTIME,
+            timePoint: b.TIMEPOINT,
+            stopId: b.STOPID,
+            blockId: b.BLOCKID,
+            tripId: b.TRIPID,
+            vechicle: b.VEHICLE
+          };
+
+          // if there is an existing bus, update it or else create and push it to the array
+          if(isPresent(bus)) {
+            bus.setProperties(attrs);
+          }else {
+            const newBusObj = busObject.create(attrs);
+            this.get('activeBuses').pushObject(newBusObj);
+          }
+          
+
         });
       }
     }
