87a0d26c434fd1122c5e95388ca96e7a5699a189
Raghuvaran <rchowda@g.clemson.edu>
2017-11-19 21:58:50 -0500

    added full screen static map

--- app/pods/components/bus-card/component.js
+++ app/pods/components/bus-card/component.js
@@ -4,4 +4,11 @@ export default Component.extend({
   tagName:'bus-card',
   // attributeBindings: ['style'],
   // style: htmlSafe('border-radius:15px;')
+
+  actions:{
+    onClck() {
+      
+      window.location.href = this.get('bus.staticmapFullImg');
+    }
+  }
 });
