405702044d9be40e2059f203662b7cc33343af51
Raghuvaran <rchowda@g.clemson.edu>
2017-11-19 19:59:12 -0500

    added static map image to bus-card

--- app/pods/components/bus-card/component.js
+++ app/pods/components/bus-card/component.js
@@ -1,4 +1,7 @@
 import Component from '@ember/component';
-
+// import { htmlSafe } from '@ember/string';
 export default Component.extend({
+  tagName:'bus-card',
+  // attributeBindings: ['style'],
+  // style: htmlSafe('border-radius:15px;')
 });
