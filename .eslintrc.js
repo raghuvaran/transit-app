04a8fbb3cdde5f838cf65ce00f09523ca5c5637b
Raghuvaran <rchowda@g.clemson.edu>
2017-11-19 20:02:11 -0500

    added new rules to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,5 +9,13 @@ module.exports = {
     browser: true
   },
   rules: {
+    "no-unused-vars": ["error", { "vars": "all", "args": "none", "ignoreRestSiblings": false }],
+    "no-console": ["error", { allow: ["warn", "error"] }],
+    "no-fallthrough": ["error", { "commentPattern": "break[\\s\\w]*omitted" }],
+    "semi": ["warn", "always", { "omitLastInOneLineBlock": true}],
+    "no-extra-semi": "off"
+  },
+  globals: {
+    moment: true,
   }
 };
