3d395b96e1cde6abe84d0eb22cf968ece03b9939
Raghuvaran <rchowda@g.clemson.edu>
2017-12-13 15:55:58 -0500

    replaced initializer with instance initializer

---
+++ app/instance-initializers/google-maps.js
@@ -0,0 +1,15 @@
+import $ from 'jquery';
+
+const key = 'AIzaSyDxkP6K5lh2VBZUVLMZW4YqvwseeDIb-PI';
+export function initialize(appInstance) {
+  // appInstance.inject('route', 'foo', 'service:foo');
+  const geoloc = appInstance.lookup('service:geoloc');
+  
+  // application.inject('route', 'foo', 'service:foo');
+  geoloc.forceGetCurrentPosition({lat: '33.766856', lng: '-84.367541'});
+  window.initMap = function() { return $.getScript(`https://maps.googleapis.com/maps/api/js?key=${key}`) };
+}
+
+export default {
+  initialize
+};
