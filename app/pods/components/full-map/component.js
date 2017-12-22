3ca4043cf538d756743338eae2b3163399a00d1d
Raghuvaran <rchowda@g.clemson.edu>
2017-12-21 22:53:44 -0500

    added userLocation to full-map

--- app/pods/components/full-map/component.js
+++ app/pods/components/full-map/component.js
@@ -1,4 +1,5 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 import {inject} from '@ember/service';
 
 
@@ -19,13 +20,40 @@ export default Component.extend({
   initMap() {
     try {
       const currentLocation = this.get('globals.currentLocation');
-      const map = new google.maps.Map(document.getElementById(this.get('elementId')), {
-        center: { lat: currentLocation.lat, lng: currentLocation.lng },
-        zoom: 16,
+      const map = new google.maps.Map(document.getElementById(`fullmap-${this.get('elementId')}`), {
+        center: { lat: parseFloat(currentLocation.lat), lng: parseFloat(currentLocation.lng) },
+        zoom: 14,
       });
       this.set('map', map);
     }catch(e) {
       console.error('Failed to init map!', e);
     }
+  },
+
+  userLocation: computed('globals.currentLocation', {
+    get() {
+      const {lat, lng} = this.get('globals.currentLocation');
+      const map = this.get('map')
+      let marker = this.get('userMarker');
+      if(!marker){
+        marker = new google.maps.Marker({
+          position: {lat, lng},
+          map: map,
+          label: 'U',
+        });
+        this.set('userMarker', marker);
+      } else {
+        marker.setPosition({lat, lng});
+      }
+
+    }
+  }),
+
+
+  willDestroyElement() {
+    this._super(...arguments);
+    const userMarker = this.get('userMarker');
+    userMarker && userMarker.setMap(null);
   }
+
 });
