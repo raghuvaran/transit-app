f87569a2dc8c7b11a6cdc6f862ec171089985ce1
Raghuvaran <rchowda@g.clemson.edu>
2017-12-26 23:32:59 -0500

    added new bus-picker

---
+++ app/pods/components/bus-picker/component.js
@@ -0,0 +1,58 @@
+import Component from '@ember/component';
+import { A as emberA} from '@ember/array';
+import { computed } from '@ember/object';
+// import EObj from '@ember/object';
+import { fetchJson } from '../../../utils/fetch';
+
+// const busObj = EObj.extend({
+//   route: null,
+//   direction: null,
+// });
+
+export default Component.extend({
+  // tagName: '',
+  init() {
+    this._super(...arguments);
+    this.getAllBuses();
+  },
+  allBuses: emberA(),
+  selectedBuses: emberA(),
+
+  remainingBuses: computed('selectedBuses.[]', 'allBuses.[]', {
+    get() {
+      const allBuses = this.get('allBuses');
+      const selectedBuses = this.get('selectedBuses');
+      return allBuses.filter(b => !selectedBuses.any(_b => _b===b))
+    }
+  }),
+
+  async getAllBuses() {
+    const url = `/assets/all-buses.json`;
+    try {
+      let buses = await fetchJson(url);
+      buses = buses.map(b =>({
+        route: parseInt(b.route),
+        direction: String(b.direction).toLowerCase()
+      }));
+      this.set('allBuses', buses);
+    } catch(e) { console.error('Failed to fetch all-buses.json')}
+  },
+  actions: {
+    addBus(bus) {
+      // const selectedBuses = this.get('selectedBuses');
+      // if(selectedBuses.indexOf(bus) == -1) {
+      //   selectedBuses.push(bus);
+      //   this.set('selectedBuses', selectedBuses);
+      // }
+      if(this.get('addBus')) return this.sendAction('addBus', bus);
+      this.get('selectedBuses').pushObject(bus);
+    },
+    removeBus(bus) {
+      // const selectedBuses = this.get('selectedBuses');
+      // const newSelection = selectedBuses.filter(b => b !== bus);
+      // this.set('selectedBuses', newSelection);
+      if(this.get('removeBus')) return this.sendAction('removeBus', bus);
+      this.get('selectedBuses').removeObject(bus);
+    }
+  }
+});
