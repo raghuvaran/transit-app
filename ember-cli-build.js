cc69c4b531b87c1fa2ad22a0f233999e8b22b7e6
rchowda <ch.raghuvaran@gmail.com>
2018-02-18 22:50:09 -0500

    add webmanifest to fingerprint exetensions

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -24,7 +24,7 @@ module.exports = function(defaults) {
   if (app.env === "production") {
     app.options.fingerprint.enabled = true;
     app.options.fingerprint.prepend = "/transit-app/";
-    app.options.fingerprint.extensions = app.options.fingerprint.extensions.concat(['json', 'svg']);
+    app.options.fingerprint.extensions = app.options.fingerprint.extensions.concat(['json', 'svg', 'webmanifest']);
     app.options.fingerprint.replaceExtensions = app.options.fingerprint.replaceExtensions.concat(['webmanifest']);
   }
 
